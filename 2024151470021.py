014c300b42bd52ea0ecd8747eb4db1a6bc5278fa
xxx <xxx>
2025-12-16 12:51:19 +0800

    delete previous uesless data

--- 2024151470021.py
+++
@@ -1,9 +0,0 @@
-a = 100
-b_list = [23, 5, 0, 76, 'A', 66]
-
-for b in b_list:
-    try:
-        result = a / b
-        print(f"{result:.4f}")
-    except Exception as e:
-        print(f"{e.__class__.__name__}: {e}")
